4c5cd1845ea186a5572f7c9dc5640c616f1a8453
Milan Harkhani <milanharkhani93@gmail.com>
2018-08-30 14:37:02 +0530

    youtube func

--- Cameo/cameo.py
+++ Cameo/cameo.py
@@ -1,10 +1,16 @@
 import cv2
-from scripts.Cameo.manager import CaptureManager,WindowManager
-from scripts.Cameo import filters
+from manager import CaptureManager,WindowManager
+import filters
+from pafy import pafy
 class Cameo(object):
-    def __init__(self):
+    def __init__(self,link=0,youtubelink=False):
+        if(youtubelink and link!=0):
+            pf = pafy.new(link)
+            best = pf.getbest(preftype="mp4")
+            link = best.url
+
         self._windowManager = WindowManager('Cameo',self.onKeyPress)
-        self._captureManager = CaptureManager(cv2.VideoCapture(0),self._windowManager,False)
+        self._captureManager = CaptureManager(cv2.VideoCapture(link),self._windowManager,False)
         self._filter = filters.BlurFilter()
     def run(self):
         self._windowManager.createWindow()
@@ -33,4 +39,4 @@ class Cameo(object):
             self._windowManager.destroyWindow()
 
 if __name__=="__main__":
-    Cameo().run()
+    Cameo(link='https://www.youtube.com/watch?v=1MVQYSlgXrI',youtubelink=True).run()
